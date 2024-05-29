43b191c11574e6117495799971a163c73d528a40
filkarnicki <filkarnicki>
2024-05-29 12:56:26 +0000

    working with charts

--- stock_charts.py
+++ stock_charts.py
@@ -1,2 +1,23 @@
 # Databricks notebook source
+import plotly.express as px
+
+df = spark.read.format("csv") \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .load("/Volumes/datasets/default/datasets/tesla_data_2020.csv")
+
+df.display()
+
+# COMMAND ----------
+
+dfp = df.toPandas()
+
+px.line(dfp, x="Date", y="Volume", title="TSLA trading volumes").show()
+
+# COMMAND ----------
+
+px.line(dfp, x="Date", y="Adj Close", title="TSLA close prices").show()
+
+# COMMAND ----------
+
 
