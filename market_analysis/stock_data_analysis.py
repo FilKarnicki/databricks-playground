0643be14450823cb2696069e00ff28d98c7ab5c7
filkarnicki <filkarnicki>
2024-05-29 12:11:41 +0000

    initial commit

---
+++ market_analysis/stock_data_analysis.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+from pyspark.sql.functions import max
+
+df = spark.read.format("csv") \
+    .option("header", "true") \
+    .load("/Volumes/datasets/default/datasets/tesla_data_full.csv")
+
+# COMMAND ----------
+
+df.display()
+
+# COMMAND ----------
+
+import pyspark.sql.functions as f
+
+df.agg(
+    f.max_by("Date", "High").alias("Date"),
+    f.max("High").alias("High"),
+    f.max_by("Low", "High").alias("Low")
+).show()
+
+
+# COMMAND ----------
+
+
